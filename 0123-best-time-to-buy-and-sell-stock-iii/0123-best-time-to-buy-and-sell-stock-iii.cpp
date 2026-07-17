d433e39e1ab6e679e11e82bc345b90b7adcd8a2e
Mohsin Hussain <103304946+MohsinHussain7728@users.noreply.github.com>
2026-07-17 15:24:18 +0530

    Time: 388 ms (52.62%), Space: 211.4 MB (46.87%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+
+    int solve(int idx, int buy, int cap, vector<int>& prices, int n, vector<vector<vector<int>>>&dp){
+        //Base Case
+        if(idx == n) return 0; //TC->O(n*2*3)
+        if(cap == 0) return 0;  //SC->O(n*2*3) + O(n)
+
+        if(dp[idx][buy][cap] != -1)
+            return dp[idx][buy][cap];        
+
+        int profit = 0;
+
+        if(buy){
+            profit = max(-prices[idx] + solve(idx+1,0,cap,prices,n,dp) ,
+                    0 + solve(idx+1, 1, cap, prices,n,dp));
+        }else{//sell
+            profit = max(prices[idx] + solve(idx+1,1,cap-1,prices,n,dp) ,
+                    0 + solve(idx+1, 0, cap, prices,n,dp));
+        }
+
+        return dp[idx][buy][cap] = profit;
+    }
+
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+
+        //3 States :-> (idx,buy,trades left);
+        // vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+        
+        //Base Case: dp array is already initialized to 0 -> covering all the base cases.
+
+        //Changing Params
+        for(int idx = n-1; idx>=0; idx--){
+            for(int buy=0; buy<2; buy++){
+                for(int cap=1; cap<3; cap++){
+                    if(buy == 1){
+                        dp[idx][buy][cap] = max(-prices[idx] + dp[idx+1][0][cap] ,
+                                            0 + dp[idx+1][1][cap]);
+                    }
+                    else{
+                        dp[idx][buy][cap] = max(prices[idx] + dp[idx+1][1][cap-1] ,
+                                            0 + dp[idx+1][0][cap]);
+                    }
+                }
+            }
+        }
+
+
+        return dp[0][1][2];
+
+
+        // return solve(0,1,2,prices,n,dp);
+    }
+};
